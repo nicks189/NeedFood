4213da126f9342bedc7bf7e7a2eef09dedd62908
Nick Steger <nsteger189@gmail.com>
2021-10-30 19:10:15 -0500

    Add log retention and remove package-lock from git

--- lib/needfood-stack.ts
+++ lib/needfood-stack.ts
@@ -1,4 +1,5 @@
 import { Code, Function, Runtime } from "@aws-cdk/aws-lambda";
+import { RetentionDays } from "@aws-cdk/aws-logs";
 import { Construct, Stack, StackProps } from "@aws-cdk/core";
 
 export class NeedfoodStack extends Stack {
@@ -8,6 +9,7 @@ export class NeedfoodStack extends Stack {
     const lookupLambda = new Function(this, 'Lookup', {
       runtime: Runtime.NODEJS_12_X,
       handler: 'Lookup.lookup',
+      logRetention: RetentionDays.THREE_MONTHS,
       code: Code.fromAsset('resources/lambda/build')
     });
   }
